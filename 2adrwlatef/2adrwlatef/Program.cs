166cd0199d366e90a67be914501c79c50e66ce1b
PC <PC@DESKTOP-7KMRJC2>
2025-10-29 01:24:21 +0200

    1

---
+++ 2adrwlatef/2adrwlatef/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _2adrwlatef
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2adr w latef w Hamdallah 3al salama"); 
+            
+        }
+    }
+}
